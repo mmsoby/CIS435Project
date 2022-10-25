c9ab6eaf3292ac2f5b2fb3e4cda098c8482774fc
Mustafa Soby <mustafasoby@icloud.com>
2022-10-25 16:09:25 -0400

    Change landing page to index.html

--- js/customElements.js
+++ js/customElements.js
@@ -3,14 +3,14 @@ class NavigationBar extends HTMLElement {
         super();
         this.innerHTML = `<nav class="navbar navbar-expand-lg" style="background-color:#00274C">
     <div class="container">
-        <a href="home.html" class="navbar-brand" style="color:#FFCB05">Better Degree Works</a>
+        <a href="index.html" class="navbar-brand" style="color:#FFCB05">Better Degree Works</a>
         <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
             <span class="navbar-toggler-icon"> </span>
         </button>
         <div class="collapse navbar-collapse" id="menu">
             <ul class="navbar-nav ms-auto ">
                 <li class="nav-item">
-                    <a href="AdvicePage.html" class="nav-link" style="color:#FFCB05">Advice</a>
+                    <a href="index.html" class="nav-link" style="color:#FFCB05">Advice</a>
                 </li>
                 <li class="nav-item ">
                     <a href="GetClasses.html" class="nav-link" style="color:#FFCB05">Get Classes</a>

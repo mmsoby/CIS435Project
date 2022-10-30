3d65986d2e20f32241757de032bb69fec3279f85
Mustafa Soby <mustafasoby@icloud.com>
2022-10-30 14:23:19 -0400

    Added questions sql connection.

---
+++ addQuestion.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "group15";
+$password = "n0bAA@efFVB";
+$dbname = "group15_db";
+$postedAt = date("Y-m-d H:i:s");
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO FAQ (question, postedAt)"
+    . "VALUES ('" . $_POST['question'] . "', '"  . $postedAt . "')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+?>

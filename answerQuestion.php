1380f738810af242aca6f10e4c4ca7d54567d896
Mustafa Soby <mustafasoby@icloud.com>
2022-10-30 19:48:51 -0400

    Finished the new answer part

---
+++ answerQuestion.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "141.215.80.154";
+$username = "group15";
+$password = "n0bAA@efFVB";
+$dbname = "group15_db";
+$postedAt = date("Y-m-d H:i:s");
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO Answers (questionID, answer)"
+    . "VALUES ('" . $_POST['questionID'] . "', '" . $_POST['answer'] . "')";
+
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+?>

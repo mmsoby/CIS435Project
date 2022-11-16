1b7f6dd4dc607bf6433512ef687c143a3cfda62c
Mustafa Soby <mustafasoby@icloud.com>
2022-11-16 18:51:03 -0500

    added multi sheet capability
    Took 9 minutes

--- listFiles.php
+++ listFiles.php
@@ -1,10 +1,20 @@
 <?php
 $files = scandir('Resources/CurriculumSheets/');
+
+echo '<div class="dropdown">
+                  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+                      <span class="caret"></span></button>
+                  <ul class="dropdown-menu">'
+
+
 foreach($files as $file) {
   //do your work here
 
   echo "<li><a href='Resources/CurriculumSheets/$file'>$file</a></li>";
 }
+
+echo '</ul></div>';
+
 // <button class="btn btn-primary text-black " style="background-color:#FFCB05 !important" type="submit">
 //                    <a style="color: inherit; text-decoration: none;"
 //                       href="Resources/CurriculumSheets/Fall2018DataScience.pdf" target="_blank"

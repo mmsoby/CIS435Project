5caa7e41f2026e0e0816f2e9ad9c3a0ff41b4d67
Mustafa Soby <mustafasoby@icloud.com>
2022-11-18 16:46:36 -0500

    began work on get classes page.
    Took 5 minutes

--- dropdown_items.php
+++ dropdown_items.php
@@ -4,11 +4,11 @@ $files = scandir('Resources/CurriculumSheets/');
 foreach($files as $file) {
   //do your work here
   if ($file != "." && $file != "..") {
-  echo "<a class= 'dropdown-item'>" . $file ."</a>";
+  echo "<option value=' ". $file ."' >" . $file . "</option>";
+  }
   }
 }
 
-echo "</div></div>";
 ?>
 
 

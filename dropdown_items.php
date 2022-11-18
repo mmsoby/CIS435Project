e7c7b42e0ffc6969082899c8a67fc3e33e9d96a7
Mustafa Soby <mustafasoby@icloud.com>
2022-11-18 17:02:05 -0500

    began work on get classes page.
    Took 3 minutes

--- dropdown_items.php
+++ dropdown_items.php
@@ -1,14 +1,13 @@
 <?php
 $files = scandir('Resources/CurriculumSheets/');
 
-echo "<select class='form-select' id='inputGroupSelect01'>
-                <option selected>Choose...</option>";
+echo "<select class='form-select' id='inputGroupSelect01'> <option selected>Choose...</option>";
 
 
 foreach($files as $file) {
   //do your work here
   if ($file != "." && $file != "..") {
-  echo "<option value=' ". $file ."' >" . $file . "</option>";
+  echo "<option value='". $file ."' >" . $file . "</option>";
   }
   }
 }

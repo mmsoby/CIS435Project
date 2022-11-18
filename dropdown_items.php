99b1133ef3f34eee3676f7829216febcc2d7db06
Mustafa Soby <mustafasoby@icloud.com>
2022-11-18 17:13:01 -0500

    began work on get classes page.
    Took 4 minutes

--- dropdown_items.php
+++ dropdown_items.php
@@ -1,7 +1,9 @@
 <?php
 $files = scandir('Resources/CurriculumSheets/');
 
-echo "<select class='form-select'>";
+echo "<select class='form-select'> <option value="0">Select a file</option>";
+
+
 
 foreach($files as $file) {
   //do your work here

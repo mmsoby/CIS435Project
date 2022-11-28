d675246beb29c6fa01731f1715d99cbe41b8e8af
Mustafa Soby <mustafasoby@icloud.com>
2022-11-28 18:58:24 -0500

    Added pdfjs dist and node modules
    Took 9 seconds

--- uploadPDF_fromUser.php
+++ uploadPDF_fromUser.php
@@ -8,7 +8,7 @@ if ( isset( $_FILES['pdfFile'] ) ) {
 		$new_str = str_replace("%20", "", $new_str);
 		$dest_file = "Resources/UserFiles/".$new_str;
 
-		echo '<head><script type="text/javascript" src="GenerateClasses.js"></script></head>';
+		echo '<head><script type="module" src="GenerateClasses.js"></script></head>';
 
         // Call the generateClasses javascript function
         echo '<script type="text/javascript">window.onload = function() {GenerateClass('.'"../Resources/UserFiles/'.$new_str.'");};</script>';

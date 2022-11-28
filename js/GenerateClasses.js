5b57929652872b62bbc1517fc2164d07cc2e9178
Mustafa Soby <mustafasoby@icloud.com>
2022-11-28 17:07:17 -0500

    Added pdfjs dist and node modules
    Took 5 minutes

--- js/GenerateClasses.js
+++ js/GenerateClasses.js
@@ -1,37 +1,31 @@
-import {pdfjsLib } from 'pdfjs-dist';
-function pdfToText(url, separator = ' ') {
-   let pdf = pdfjsLib.getDocument(url);
-   return pdf.promise.then(function(pdf) { // get all pages text
-      let maxPages = pdf._pdfInfo.numPages;
-      let countPromises = []; // collecting all page promises
-      for (let i = 1; i <= maxPages; i++) {
-         let page = pdf.getPage(i);
-         countPromises.push(page.then(function(page) { // add page promise
-            let textContent = page.getTextContent();
-            return textContent.then(function(text) { // return content promise
-               return text.items.map(function(obj) {
-                  return obj.str;
-               }).join(separator); // value page text
-            });
-         }));
-      };
-      // wait for all pages and join text
-      return Promise.all(countPromises).then(function(texts) {
-         for(let i = 0; i < texts.length; i++){
-            texts[i] = texts[i].replace(/\s+/g, ' ').trim();
-         };
-         return texts;
-      });
-   });
+import PDFJS from "pdfjs-dist";
+import PDFJSWorker from "pdfjs-dist/build/pdf.worker.js"; // add this to fit 2.3.0
+
+PDFJS.disableTextLayer = true;
+PDFJS.disableWorker = true; // not availaible anymore since 2.3.0 (see imports)
+
+const getPageText = async (pdf, pageNo) => {
+   const page = await pdf.getPage(pageNo);
+   const tokenizedText = await page.getTextContent();
+   return tokenizedText.items.map(token => token.str).join("");
+};
+
+/* see example of a PDFSource below */
+export const getPDFText = async (source)=> {
+   Object.assign(window, {pdfjsWorker: PDFJSWorker}); // added to fit 2.3.0
+   const pdf= await PDFJS.getDocument(source).promise;
+   const maxPages = pdf.numPages;
+   const pageTextPromises = [];
+   for (let pageNo = 1; pageNo <= maxPages; pageNo += 1) {
+      pageTextPromises.push(getPageText(pdf, pageNo));
+   }
+   const pageTexts = await Promise.all(pageTextPromises);
+   return pageTexts.join(" ");
 };
 
 function GenerateClass (pdfURL) {
-   pdfToText(pdfURL).then(function(pdfTexts) {
-      console.log(pdfTexts);
-      // RESULT: ['TEXT-OF-PAGE-1', 'TEXT-OF-PAGE-2', ...]
-   }, function(reason) {
-      console.error(reason);
+   getPageText(pdfURL,1).then((text) => {
+        console.log(text);
    });
-
    console.log("url is " + pdfURL);
 }
